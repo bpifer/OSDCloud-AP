e30228667a1c78d3820eb5c1d09aa6de434eca6b
Brendon Pifer <bpifer@usw.org>
2025-09-09 10:33:19 -0400

    Testing osd logging

--- OSDCloud-AP.ps1
+++ OSDCloud-AP.ps1
@@ -2,6 +2,15 @@
 Start-Transcript -Path X:\OSDCloud\OSDBuild.log -ErrorAction Ignore
 Write-Host -ForegroundColor DarkGray "Start USW OSDCloud Build"
 Write-Host "System drive = $env:SystemDrive"
+
+#Make sure I have the latest OSD Content
+Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
+Import-Module OSD -Force
+#Start-Sleep -Seconds 5
+
 #endregion
 
 #region WinPE
@@ -9,7 +18,7 @@ if ($env:SystemDrive -eq 'X:') {
     osdcloud-StartWinPE -OSDCloud -KeyVault
     #Write-Host -ForegroundColor Cyan "To start a new PowerShell session, type 'start powershell' and press enter"
     Write-Host -ForegroundColor Cyan "Starting OSDCloud install"
-    Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Enterprise -OSActivation Volume -ZTI -Restart -SkipAutopilot
+    Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Enterprise -OSActivation Volume -ZTI -Restart -SkipAutopilot 
 }
 #endregion
 
