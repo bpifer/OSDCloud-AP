2c2948631589df60c136243d45ac2071db11b3e7
Brendon Pifer <bpifer@usw.org>
2025-09-09 10:50:39 -0400

    Testing osd logging

--- OSDCloud-AP.ps1
+++ OSDCloud-AP.ps1
@@ -18,6 +18,7 @@ if ($env:SystemDrive -eq 'X:') {
     osdcloud-StartWinPE -OSDCloud -KeyVault
     #Write-Host -ForegroundColor Cyan "To start a new PowerShell session, type 'start powershell' and press enter"
     Write-Host -ForegroundColor Cyan "Starting OSDCloud install"
+    
     Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Enterprise -OSActivation Volume -ZTI -SkipAutopilot 
     
     Get-WindowsAutopilotInfo -Online
