6e54cb960b5eca63e3dd18f7b56ca8d12637f1a2
Brendon Pifer <b.pifer@gmail.com>
2025-10-29 08:53:06 -0400

    Update OSDCloud-AP.ps1

--- OSDCloud-AP.ps1
+++ OSDCloud-AP.ps1
@@ -1,13 +1,13 @@
 #region Initialize
 Start-Transcript -Path X:\OSDCloud\OSDBuild.log -ErrorAction Ignore
-Write-Host -ForegroundColor DarkGray "Start USW OSDCloud Build"
+Write-Host -ForegroundColor DarkGray "Start OSDCloud Build"
 Write-Host "System drive = $env:SystemDrive"
 
 #Make sure I have the latest OSD Content
-Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
+Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
 Install-Module OSD -Force -ErrorAction SilentlyContinue
 
-Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
+Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
 Import-Module OSD -Force -ErrorAction SilentlyContinue
 #Start-Sleep -Seconds 5
 
