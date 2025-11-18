9f45779a4281a3720f8689d7cc4cf55b59b8eb60
Brendon Pifer <b.pifer@gmail.com>
2025-11-18 17:52:08 -0500

    Update OSDCloud-AP.ps1

--- OSDCloud-AP.ps1
+++ OSDCloud-AP.ps1
@@ -1,25 +1,10 @@
-#region Initialize
 Start-Transcript -Path X:\OSDCloud\OSDBuild.log -ErrorAction Ignore
 Write-Host -ForegroundColor DarkGray "Start OSDCloud Build"
 Write-Host "System drive = $env:SystemDrive"
 
-#Make sure I have the latest OSD Content
-Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
-Install-Module OSD -Force -ErrorAction SilentlyContinue
-
-Write-Host  -ForegroundColor Cyan "Importing OSD PowerShell Module"
-Import-Module OSD -Force -ErrorAction SilentlyContinue
-#Start-Sleep -Seconds 5
-
-#endregion
-
-#region WinPE
 if ($env:SystemDrive -eq 'X:') {
     #Write-Host -ForegroundColor Cyan "To start a new PowerShell session, type 'start powershell' and press enter"
     Write-Host -ForegroundColor Cyan "Starting OSDCloud install"
 
     Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Enterprise -OSActivation Volume -ZTI -Restart
 }
-
-#endregion
-
